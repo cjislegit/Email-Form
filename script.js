35d5ed96e23f9f7fe7fcc09dda725541a34c00de
cjislegit <carlosjramirez11@gmail.com>
2018-06-11 12:50:21 -0700

    Added validation to make sure the form is filled out correctly and prevent it frome being sent if not

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 $('#submit').click(function(){
     var errorMessage = "";
 
-    if(validateEmail($('#inputEmail')) == false){
-        errorMessage += "<br>Please Enter a Valid Email";
-    }
+    $("form").submit(function(e) {
 
-    if($('#inputSubject').val() == ""){
-        errorMessage += "<br> The subject field is required";
-    }
+        if($('#inputEmail').val() == ""){
+            errorMessage += "<br> The email field is required";
+        }
 
-    if($('#inputMessage').val() == ""){
-        errorMessage += "<br> The content field is required";
-    }
+        if($('#inputSubject').val() == ""){
+            errorMessage += "<br> The subject field is required";
+        }
+    
+        if($('#inputMessage').val() == ""){
+            errorMessage += "<br> The content field is required";
+        }
 
-    if(errorMessage != ""){
-        $("#errorMessage").html('<div class="alert alert-danger"><strong>There were error(s) in your form:</strong>' + errorMessage + "</div>");
-    }
-});
+        if(errorMessage != ""){
+            $("#errorMessage").html('<div class="alert alert-danger"><strong>There were error(s) in your form:</strong>' + errorMessage + "</div>")
+            return false;
+        }else{
+            return true;
+        }
+    });
 
-function validateEmail(email) {
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    return emailReg.test(email);
-  }
-
-  $("#form").submit(function(e) {
-    e.preventDefault();
 });
